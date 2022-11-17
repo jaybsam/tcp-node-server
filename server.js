d0d75400083619e9d53d0bf3b138dcf5d26d9f05
Joshua Sam <31703272+jaybsam@users.noreply.github.com>
2022-11-18 00:55:09 +0800

    Added server tcp files

--- server.js
+++ server.js
@@ -1,47 +1,32 @@
-
-'use strict';
- 
 const net = require('net');
-const PORT = 1234;
-const HOST = 'localhost';
- 
-class Client {
- constructor(port, address) {
-  this.socket = new net.Socket();
-  this.address = address || HOST;
-  this.port = port || PORT;
-  this.init();
- }
- 
- init() {
-  var client = this;
-  client.socket.connect(client.port, client.address, () => {
-   console.log(`Client connected to: ${client.address} :  ${client.port}`);
-  });
- 
-  client.socket.on('close', () => {
-   console.log('Client closed');
-  });
- }
- 
- sendMessage(message) {
-  var client = this;
-  return new Promise((resolve, reject) => {
- 
-   client.socket.write(message);
- 
-   client.socket.on('data', (data) => {
-    resolve(data);
-    if (data.toString().endsWith('exit')) {
-     client.socket.destroy();
-    }
-   });
- 
-   client.socket.on('error', (err) => {
-    reject(err);
-   });
- 
-  });
- }
-}
-module.exports = Client;
+const port = 7070;
+const host = '127.0.0.1';
+
+const server = net.createServer();
+server.listen(port, host, () => {
+    console.log('TCP Server is running on port ' + port + '.');
+});
+
+let sockets = [];
+
+server.on('connection', function(sock) {
+    console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
+    sockets.push(sock);
+
+    sock.on('data', function(data) {
+        console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        // Write the data back to all the connected, the client will receive it as data from the server
+        sockets.forEach(function(sock, index, array) {
+            sock.write(sock.remoteAddress + ':' + sock.remotePort + " said " + data + '\n');
+        });
+    });
+
+    // Add a 'close' event handler to this instance of socket
+    sock.on('close', function(data) {
+        let index = sockets.findIndex(function(o) {
+            return o.remoteAddress === sock.remoteAddress && o.remotePort === sock.remotePort;
+        })
+        if (index !== -1) sockets.splice(index, 1);
+        console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
+    });
+});
